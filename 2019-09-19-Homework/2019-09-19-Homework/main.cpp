d4c0a6f1b85c53bed9321b8bb66540972b6a0ab1
tacjoseph <55443251+tacjoseph@users.noreply.github.com>
2019-09-19 08:29:04 -0400

    Add files via upload

---
+++ 2019-09-19-Homework/2019-09-19-Homework/main.cpp
@@ -0,0 +1,12 @@
+// Name: Joseph Loiseau
+// Intro into Computer Science
+// 2019-09-19-Homework
+// Period 1
+// Mr.Williams
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Joseph Loiseau \nClass Schedule \nPeriod1 Computer Scince \nPeriod2 Spanish2 \nPeriod3 Pyshical Education \nPeriod4 Earth Science \nPeriod5 Algebra2 cc H \nPeriod6 Health \nPeriod7 Lunch \nPeriod8 Global History \nPeriod9 English10 Honors \nPeriod10 House";
+}
